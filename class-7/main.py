cb9336d58a59374ff669c5daf4d5ae6048793535
ヘゲディス・ジョシュア <josh.hegedus@outlook.com>
2024-11-21 21:16:01 +0100

    added class 8

--- class-7/main.py
+++ class-7/main.py
@@ -1,21 +1,39 @@
 from fastapi import FastAPI
 from typing import Literal
+import wikipedia
 
-app = FastAPI()
+tags_metadata = [
+    {
+        "name": "Demo",
+        "description": "API endpoints for demo purposes",
+    },
+    {
+        "name": "Home",
+        "description": "Basic endpoints"
+    },
+    {
+        "name": "AI",
+        "description": "AI operations"
+    }
+]
 
-@app.get("/")
+app = FastAPI(
+    openapi_tags=tags_metadata,
+)
+
+@app.get("/", tags=["Home"])
 def home():
     return {"message": "Hello World!"}
 
-@app.get("/about")
+@app.get("/about", tags=["Home"])
 def about():
     return {
-        "Author": "Joshua Hegedus",
+        "Author": "Joshua Hegedus @ home",
         "Type": "Logiscool Project",
         "Description": "An AI API collection."
     }
 
-@app.get("/calculate")
+@app.get("/calculate", tags=["Demo"])
 def calculate(a: int, b: int):
     result = a + b
 
@@ -25,7 +43,7 @@ def calculate(a: int, b: int):
         "result": result
     }
 
-@app.get("/calculate/{a}/{b}")
+@app.get("/calculate/{a}/{b}", tags=["Demo"])
 def complex_calculate(a: int, b: int, operator: Literal["+", "-", "*", "/"] = "+"):
 
     match operator:
@@ -52,19 +70,18 @@ class CalcReqeust(BaseModel):
     b: int
     operator: str = "+"
 
-@app.post("/calculate")
+@app.post("/calculate", tags=["Demo"])
 def caluclate_post(req: CalcReqeust):
     a, b, operator = req.a, req.b, req.operator
 
-    match operator:
-        case "+":
-            result = a + b
-        case "-":
-            result = a - b
-        case "*":
-            result = a * b
-        case "/":
-            result = a / b
+    if operator == "+":
+        result = a + b
+    elif operator == "-":
+        result = a - b
+    elif operator == "*":
+        result = a * b
+    elif operator == "/":
+        result = a / b
 
     return {
         "a": a,
@@ -73,3 +90,33 @@ def caluclate_post(req: CalcReqeust):
         "result": result if result else "Some of the input were incorrect!"
     }
 
+
+class AIOperation(BaseModel):
+    text: str
+
+
+@app.post("/ai/summary", tags=["AI"])
+def ai_summary(req: AIOperation):
+    return {"text": req.text}
+
+
+@app.get("/ai/wikipedia/search", tags=["AI"])
+def wikipedia_search(search_term: str, lang: str = "en"):
+    wikipedia.set_lang(lang)
+    results = wikipedia.search(search_term)
+    if len(results) == 0:
+        return {
+            "search_term": search_term,
+            "result": "No results found"
+        }
+    if results[0] == search_term:
+        page = wikipedia.page(results[0])
+        return {
+            "search_term": search_term,
+            "result": page.content
+        }
+
+    return {
+        "search_term": search_term,
+        "result": results
+    }
