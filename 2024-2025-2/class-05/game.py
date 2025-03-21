cca41f161e8c1b68d8209ca02a28a540167ebdee
ヘゲディス・ジョシュア <josh.hegedus@outlook.com>
2025-03-21 18:46:34 +0100

    feat: progress

--- 2024-2025-2/class-05/game.py
+++ 2024-2025-2/class-05/game.py
@@ -1,27 +1,41 @@
 import random
 import time
-import curses
+import os
+from blessed import Terminal
+
+os.environ['TERM'] = 'xterm'
+
+class Pos:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
 
 
 class Tile:
-    def __init__(self, symbol, walkable):
+    def __init__(self, symbol, walkable, pos: Pos):
         self.symbol = symbol
         self.walkable = walkable
+        self.pos = pos
+
+
+class Entity(Tile):
+    def __init__(self, symbol, walkable, pos: Pos):
+        super().__init__(symbol, walkable, pos)
 
 
 class Wall(Tile):
-    def __init__(self):
-        super().__init__("#", False)
+    def __init__(self, pos):
+        super().__init__("#", False, pos)
 
 
 class Empty(Tile):
-    def __init__(self):
-        super().__init__(" ", True)
+    def __init__(self, pos):
+        super().__init__(" ", True, pos)
 
 
 class Treasure(Tile):
-    def __init__(self):
-        super().__init__("T", True)
+    def __init__(self, pos):
+        super().__init__("T", True, pos)
         self.collected = False
 
     def collect(self):
@@ -30,18 +44,18 @@ class Treasure(Tile):
 
 
 class Exit(Tile):
-    def __init__(self):
-        super().__init__("E", True)
+    def __init__(self, pos):
+        super().__init__("E", True, pos)
 
 
-class Player(Tile):
-    def __init__(self):
-        super().__init__("P", True)
+class Player(Entity):
+    def __init__(self, pos):
+        super().__init__("P", True, pos)
 
 
-class Enemy(Tile):
-    def __init__(self):
-        super().__init__("E", True)
+class Enemy(Entity):
+    def __init__(self, pos):
+        super().__init__("E", True, pos)
 
 
 class Game:
@@ -49,16 +63,18 @@ class Game:
         self.width = width
         self.height = height
         self.map = []  # type: list[list[Tile]]
-        for _ in range(height):
+        for i in range(height):
             tmp = []
-            for _ in range(width):
-                tmp.append(Empty())
+            for j in range(width):
+                pos = Pos(j, i)
+                tmp.append(Empty(pos)) # !!!! CHANGED !!!!!
             self.map.append(tmp)
         # self.map = [[Empty() for _ in range(self.width)] for _ in range(self.height)]
-        self.player_pos = [1, 1]
+        
         self.enemies = []
         self.treasure = None
-        self.exit_pos = (width - 2, height - 2)
+        self.exit_pos = Pos(width - 2, height - 2)
+        self.player = Player(Pos(1, 1))
         self._init_map()
 
     def _init_map(self):
@@ -66,27 +82,26 @@ class Game:
         self._place_treasure()
         self._place_enemies()
 
-        self.map[1][1] = Player()
-        self.map[self.exit_pos[1]][self.exit_pos[0]] = Exit()
+        self.map[self.exit_pos.y][self.exit_pos.x] = Exit(self.exit_pos)
 
     def _place_walls(self):
         for i in range(self.width):
-            self.map[0][i] = Wall()
-            self.map[self.height - 1][i] = Wall()
+            self.map[0][i] = Wall(Pos(i, 0))
+            self.map[self.height - 1][i] = Wall(Pos(i, self.height - 1))
 
         for i in range(1, self.height - 1):
-            self.map[i][0] = Wall()
-            self.map[i][self.width - 1] = Wall()
+            self.map[i][0] = Wall(Pos(0, i))
+            self.map[i][self.width - 1] = Wall(Pos(self.width - 1, i))
 
         for _ in range((self.width * self.height) // 6):
             x, y = random.randint(1, self.width - 2), random.randint(1, self.height - 2)
-            self.map[y][x] = Wall()
+            self.map[y][x] = Wall(Pos(x, y))
 
     def _place_treasure(self):
         while True:
             x, y = random.randint(1, self.width - 2), random.randint(1, self.height - 2)
             if isinstance(self.map[y][x], Empty):
-                self.map[y][x] = Treasure()
+                self.map[y][x] = Treasure(Pos(x, y))
                 self.treasure = (x, y)
                 break
 
@@ -95,7 +110,7 @@ class Game:
             while True:
                 x, y = random.randint(1, self.width - 2), random.randint(1, self.height - 2)
                 if isinstance(self.map[y][x], Empty):
-                    self.map[y][x] = Enemy()
+                    self.map[y][x] = Enemy(Pos(x, y))
                     self.enemies.append([x, y])
                     break
 
@@ -103,47 +118,52 @@ class Game:
         pass
 
     def _move_player(self, dx, dy):
-        x, y = self.player_pos[0] + dx, self.player_pos[1] + dy
+        x, y = self.player.pos.x + dx, self.player.pos.y + dy
         tile = self.map[y][x]
+        if isinstance(tile, Treasure):
+            tile.collect()
         if tile.walkable:
-            self.map[self.player_pos[1]][self.player_pos[0]] = Empty()
-            self.player_pos = [x, y]
-            self.map[y][x] = Player()
+            self.map[self.player.pos.y][self.player.pos.x] = Empty(self.player.pos)
+            self.player.pos = Pos(x, y)
 
     def _check_victory(self):
         pass
 
-    def _draw(self, screen):
-        screen.clear()
+    def _draw(self, term: Terminal):
+        term.clear()
+
         for y in range(self.height):
             for x in range(self.width):
-                screen.addch(y, x * 2, self.map[y][x].symbol)
-        screen.refresh()
+                term.move(y, x * 2)
+                term.addstr(self.map[y][x].symbol)
+        px, py = self.player.pos.x, self.player.pos.y
+        term.move(py, px * 2)
+        term.addstr(self.player.symbol)
 
-    def play(self, screen):
-        curses.curs_set(0)
-        screen.nodelay(1)
-        screen.timeout(200)
+    def play(self, term: Terminal):
+        term.hidden_cursor()
 
         while True:
-            self._draw(screen)
-            key = screen.getch()
-
-            if key in [ord("w"), curses.KEY_UP]:
+            self._draw(term)
+            with term.cbreak(), term.hidden_cursor():
+                inp = term.inkey()
+            if inp == "w":
                 self._move_player(0, -1)
-            elif key in [ord("s"), curses.KEY_DOWN]:
+            elif inp == "s":
                 self._move_player(0, 1)
-            elif key in [ord("a"), curses.KEY_LEFT]:
+            elif inp == "a":
                 self._move_player(-1, 0)
-            elif key in [ord("d"), curses.KEY_RIGHT]:
+            elif inp == "d":
                 self._move_player(1, 0)
-            elif key == ord("q"):
+            elif inp == "q":
                 break
 
 
 def main():
     game = Game(20, 20)
-    curses.wrapper(game.play)
+    term = Terminal()
+    game.play(term)
+
 
 
 if __name__ == '__main__':
